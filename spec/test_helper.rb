d10d3317c44a405e9f47afa780d975bcd531b453
Igor Escobar <blog@igorescobar.com>
2015-06-02 22:41:11 -0300

    rspec helper and test helpers

---
+++ spec/test_helper.rb
@@ -0,0 +1,24 @@
+module TestHelpers
+
+  def capture_stdout(&block)
+    original_stdout = $stdout
+    $stdout = fake = StringIO.new
+    begin
+      yield
+    ensure
+      $stdout = original_stdout
+    end
+    fake.string
+  end
+
+  def capture_stderr(&block)
+    original_stderr = $stderr
+    $stderr = fake = StringIO.new
+    begin
+      yield
+    ensure
+      $stderr = original_stderr
+    end
+    fake.string
+  end
+end
