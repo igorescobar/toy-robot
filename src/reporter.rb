9eed94babee5d8e02941aa86350f186a5f862770
Igor Escobar <blog@igorescobar.com>
2015-06-02 22:41:24 -0300

    reporter implementation

---
+++ src/reporter.rb
@@ -0,0 +1,10 @@
+class Reporter
+  attr_reader :router
+  def initialize(router)
+    @router = router
+  end
+
+  def report
+    puts [@router.x, @router.y, @router.facing.to_s.upcase].join(",")
+  end
+end
