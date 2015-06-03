c6a480aebbcb304b7b719b310c20e6721f5cd109
Igor Escobar <blog@igorescobar.com>
2015-06-02 22:41:31 -0300

    robot implementation

---
+++ src/robot.rb
@@ -0,0 +1,28 @@
+require "./src/reporter"
+require "./src/interface"
+require "./src/router"
+
+class Robot < Interface
+
+  attr_accessor :x, :y, :facing, :router
+
+  def initialize(x = 0, y = 0, facing = :north, router=Router)
+    @router = router.new x, y, facing
+    @facing = @router.facing
+    @x = @router.x
+    @y = @router.y
+    @directions = @router.directions
+  end
+
+  def x
+    @router.x
+  end
+
+  def y
+    @router.y
+  end
+
+  def facing
+    @router.facing
+  end
+end
